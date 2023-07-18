be2e9a3a270f672b6b1e233fc9a1daedde1dd44d
noahl25 <97693037+noahl25@users.noreply.github.com>
2023-07-17 18:03:12 -0700

    Update mtbakerparser.py

--- ESR_LSRI/mtbakerparser.py
+++ ESR_LSRI/mtbakerparser.py
@@ -34,14 +34,12 @@ def getiButtonNum(site, desc, year = "All"): #All: for debug purposes
   
     return iButtons.at[getIndexFromDesc(site, desc), year + " #"]
     
-def getiButtonData(site, desc, year: str):
+def getiButtonData(site: str, desc: str, year: str):
     
     if desc not in ["Buried", "Shaded", "Exposed"] or year not in ["2018", "2019", "2020", "2021", "2022"]:
         raise Exception()
-    try:
-        return pd.read_csv("data//" + year + "//" + "MBCP" + year + "-" + str(int(year) + 1) + "_iButton" + getiButtonNum(site, desc, str(year)) + ".csv", skiprows=14, encoding="latin1")
-    except:
-        raise Exception("Missing data.")
+    return pd.read_csv("data//" + year + "//" + "MBCP" + year + "-" + str(int(year) + 1) + "_iButton" + getiButtonNum(site, desc, str(year)) + ".csv", skiprows=14, encoding="latin1")
+    
 
 def getAltitude(iButtonNum, year: str):
     
@@ -55,31 +53,47 @@ def getAltitude(iButtonNum, year: str):
     
     raise Exception("No altitude data.")
 
-def getDataYears(site, desc, yrRange: str) -> list:
+def toDecimalDate(date: str):
+    
+    x = date.split(" ")[0].split("/")
+    
+    return int(x[0])/12 + int(x[1])/365 + int(x[2])
+                              
+def getDataYears(site, desc, yrRange: str, ignoreMissingData: bool) -> list:
     
     years = yrRange.split("-")
     data = []
-    
-    print(years)
+    times = []
     
     num = years[0]
 
-    while (True):
+    while (int(num) < int(years[1])):
 
-        data += getiButtonData(site, desc, num)["Value"].tolist()
-
+        try:
+            data += getiButtonData(site, desc, num)["Value"].tolist()
+            times += getiButtonData(site, desc, num)["Date/Time"].tolist()
+        except:
+            
+            if not ignoreMissingData:
+                raise Exception("Missing date from site {}, {}, {}, num {}.".format(site, desc, num, getiButtonNum(site, desc, num)))
+                
+        
         num = int(num) + 1
         num = str(num)
-        
-        if (int(num) > int(years[1])):
-            
-            break
     
-    return data
+    return data, times
 
-pt.plot(getDataYears(1, "Buried", "2018-2021"))
 
-pt.xlabel("Time")
+#inputs: site, description (Buried, Exposed, Shaded), year range, false to throw an exception when
+#missing data, true to just ignore it
+data, times = getDataYears(1, "Buried", "2018-2021", False)
+
+times = [round(toDecimalDate(x), 2) for x in times]
+
+#you can also do pt.plot(times, data) and mess around with that
+pt.plot(times, data)
+
+pt.xlabel("Decimal Date")
 pt.ylabel("Celcius")
 
 pt.show()
